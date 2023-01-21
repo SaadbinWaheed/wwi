a9383bcb40c17b8aab838adf6bb3dcf2377531ea
SaadBinWaheed-empglabs <saad.waheed@empglabs.com>
2023-01-21 20:14:50 +0500

    text tile added

--- src/Components/AllTabs/Home.js
+++ src/Components/AllTabs/Home.js
@@ -12,7 +12,17 @@ export const Home = ({id, currentTab}) =>{
             </div>
             <div className="imageRow">
                 <img  className="image" src={bottomLeftLogo} alt="logo" />
-                <img  className="image" src={topLeftLogo} alt="logo" />
+                <div className='textTile'>
+                    <div className='textContent'>
+                        <p className='textSection'>
+                            It really is not about us, it is about YOU.
+                        </p>
+                        <p className='textSection'>
+                            We believe that you as a woman should be able to excel in your career and be a wife, mother, daughter or any other roles you desire. Having a career should not get in the way of you being you.
+                        </p>
+                        <button className='readMoreBtn' title='Read More'>Read More</button>
+                    </div>
+                </div>
             </div>
         </div>
     ): null
